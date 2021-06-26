019f8d0857ade364f80971c6adef2576a340456c
Tommy Hoang <tommyyhoangg@gmail.com>
2021-06-26 17:57:04 +0200

    Added add new post with textarea that scales with text

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaPencilAlt } from 'react-icons/fa';
 import './home.scss';
 import Header from '../../components/Header/Header';
 import Posts from '../../components/Posts/Posts';
+import NewPost from './component/NewPost';
 
 const Home = () => {
+  const [newPost, setNewPost] = useState(false);
+
+  if (newPost) {
+    document.body.style.overflowY = 'hidden';
+  } else {
+    document.body.style.overflowY = 'initial';
+  }
+
   return (
     <>
       <Header />
       <main>
         <Posts />
+        <div className='new_post'>
+          <FaPencilAlt size='1.5rem' onClick={() => setNewPost(!newPost)} />
+        </div>
+        <NewPost newPost={newPost} setNewPost={setNewPost} />
       </main>
     </>
   );
