0b31154f59dae51b8f6496be20271bc7f3ac7b83
Tommy Hoang <tommyyhoangg@gmail.com>
2021-07-03 18:20:51 +0200

    Added 404 page

--- client/src/App.js
+++ client/src/App.js
@@ -5,18 +5,22 @@ import Saved from './pages/Saved/Saved';
 import Inbox from './pages/Inbox/Inbox';
 import DirectMsg from './pages/DirectMsg/DirectMsg';
 import Profile from './pages/Profile/Profile';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import NotFound from './pages/NotFound/NotFound';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-      <Route component={Home} path='/' exact />
-      <Route component={Login} path='/login' exact />
-      <Route component={Register} path='/register' exact />
-      <Route component={Saved} path='/saved' exact />
-      <Route component={Inbox} path='/inbox' exact />
-      <Route component={DirectMsg} path='/inbox/:username' exact />
-      <Route component={Profile} path='/profile/:username' exact />
+      <Switch>
+        <Route component={Home} path='/' exact />
+        <Route component={Login} path='/login' exact />
+        <Route component={Register} path='/register' exact />
+        <Route component={Saved} path='/saved' exact />
+        <Route component={Inbox} path='/inbox' exact />
+        <Route component={DirectMsg} path='/inbox/:username' exact />
+        <Route component={Profile} path='/profile/:username' exact />
+        <Route component={NotFound} path='*' exact />
+      </Switch>
     </Router>
   );
 }
---
+++ client/src/pages/NotFound/NotFound.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import './notFound.scss';
+import { Link } from 'react-router-dom';
+import { FaChevronLeft } from 'react-icons/fa';
+import { HelmetProvider, Helmet } from 'react-helmet-async';
+import Loading from '../../components/Loading/Loading';
+import Header from '../../components/Header/Header';
+
+const NotFound = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(false);
+  }, []);
+
+  if (loading) return <Loading />;
+
+  return (
+    <>
+      <HelmetProvider>
+        <Helmet>
+          <title>Page Not Found • Frievel</title>
+        </Helmet>
+      </HelmetProvider>
+      <Header />
+      <main className='notFound'>
+        <h1>
+          <span>We're sorry, but the</span>
+          <span>page you’re looking for cannot be found</span>
+        </h1>
+        <div className='goBack_link'>
+          <FaChevronLeft fill='#1e82df' size='1rem' />
+          <Link to='/'>Go Back Home</Link>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default NotFound;
