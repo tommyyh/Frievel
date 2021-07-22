ccef92692d28a4d8a5b8bdbeef627e1a54a8f3d5
Tommy Hoang <tommyyhoangg@gmail.com>
2021-07-22 21:06:29 +0200

    Added redis

--- Frievel/settings.py
+++ Frievel/settings.py
@@ -145,6 +145,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'client/build/static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'client/build/static')
