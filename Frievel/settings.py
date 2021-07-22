5cc83cb6a3e949a9f131b6087da02e3b8c2526c6
Tommy Hoang <tommyyhoangg@gmail.com>
2021-07-22 19:24:48 +0200

    Added whitenoise for heroku static files

--- Frievel/settings.py
+++ Frievel/settings.py
@@ -44,6 +44,7 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -145,6 +146,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'client/build/static')
