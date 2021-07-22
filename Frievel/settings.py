e6eb9ef5a1423b874468363d42d59ff340c195e7
Tommy Hoang <tommyyhoangg@gmail.com>
2021-07-23 01:20:52 +0200

    Added daphne

--- Frievel/settings.py
+++ Frievel/settings.py
@@ -1,6 +1,12 @@
 import os
 from pathlib import Path
+import django
 from decouple import config
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = config('SECRET_KEY')
+
+django.setup()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,8 +15,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!j=o2!5giq4_lb&-@6nyohi-$smw!e)ixp^gc@%ahn_vxjw0ka'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
