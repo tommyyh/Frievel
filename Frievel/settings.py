c881d8c3d417a2164e5b5da5178f4c6c5a96540d
Tommy Hoang <tommyyhoangg@gmail.com>
2021-07-22 21:10:37 +0200

    Added redis

--- Frievel/settings.py
+++ Frievel/settings.py
@@ -147,9 +147,6 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'client/build/static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'client/build/static')
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
